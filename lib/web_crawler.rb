b5ad289861d0e7419dd1c419df9fc6aba39e0dba
Benjamin Kies <ben@joingrouper.com>
2014-08-22 16:02:39 -0400

    Added spec file and main crawler file

---
+++ lib/web_crawler.rb
@@ -0,0 +1,11 @@
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+# A class for parsing a url
+class WebCrawler
+end
+
+class Page
+end
---
+++ specs/web_crawler_specs.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+require_relative "../lib/web_crawler"
+
+describe WebCrawler do
+
+end
+
+describe Page do
+
+end
