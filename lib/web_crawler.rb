e8efcbeb478cf8b3d00750be03510ca14e1787ad
Benjamin Kies <ben@joingrouper.com>
2014-08-25 10:37:19 -0400

    Addded some basic tests
    And started implementing the crawler

--- lib/web_crawler.rb
+++ lib/web_crawler.rb
@@ -5,7 +5,68 @@ require 'open-uri'
 
 # A class for parsing a url
 class WebCrawler
+
+  attr_accessor :pages, :url, :queue
+
+  def self.crawl(url)
+    crawler = self.new(url)
+    crawler.queue << url
+    crawler.crawl
+    crawler
+  end
+
+  def initialize(url)
+    @url = url
+    @queue = []
+    @pages = {}
+  end
+
+  def crawl
+    return unless url = @queue.pop
+    page = Page.parse(url)
+    @queue += page.links.reject { |link| @pages[link].nil? }
+  end
 end
 
 class Page
+
+  attr_accessor :url, :links, :html, :js, :css, :images
+
+  def self.parse(url)
+    page = self.new url
+    page.parse
+    page
+  end
+
+  def initialize(url)
+    @url = url
+    @js = []
+    @css = []
+    @images = []
+    @links = []
+  end
+
+  def parse
+    @html = Nokogiri::HTML(open_url)
+  end
+
+  def parse_links
+    @links = @html.css('a').map { link['href'] }
+  end
+
+  def parse_css
+    @css = @html.css('link').map { link['href'] }
+  end
+
+  def parse_js
+    @js = @html.css('script').map { link['src'] }
+  end
+
+  def parse_images
+    @images = @html.css('img').map { link['src'] }
+  end
+
+  def open_url
+    open url
+  end
 end
--- specs/web_crawler_specs.rb
+++ specs/web_crawler_specs.rb
@@ -4,8 +4,22 @@ require_relative "../lib/web_crawler"
 
 describe WebCrawler do
 
+  describe '#parse' do
+    let(:url) { 'http://google.com' }
+
+    it "returns a crawler" do
+      WebCrawler.crawl(url).must_be_instance_of WebCrawler
+    end
+  end
 end
 
 describe Page do
 
+  describe '#parse' do
+    let(:url) { 'http://google.com' }
+
+    it "returns a Page" do
+      Page.parse(url).must_be_instance_of Page
+    end
+  end
 end
