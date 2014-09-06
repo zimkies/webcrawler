7e1960ed0ae5e4a7c74e1dafd27d7448e10dfadc
Benjamin Kies <ben@joingrouper.com>
2014-09-06 18:00:27 -0400

    parse internal links

--- lib/web_crawler.rb
+++ lib/web_crawler.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 # A class for parsing a url
 class WebCrawler
@@ -22,15 +23,19 @@ class WebCrawler
   end
 
   def crawl
-    return unless url = @queue.pop
-    page = Page.parse(url)
-    @queue += page.links.reject { |link| @pages[link].nil? }
+    while url = @queue.shift
+      p "url #{url}"
+      page = Page.parse(url)
+      @queue += page.internal_links.select do |link|
+        @pages[link].nil?
+      end
+    end
   end
 end
 
 class Page
 
-  attr_accessor :url, :links, :html, :js, :css, :images
+  attr_accessor :url, :raw_links, :html, :js, :css, :images
 
   def self.parse(url)
     page = self.new url
@@ -47,26 +52,46 @@ class Page
   end
 
   def parse
+
     @html = Nokogiri::HTML(open_url)
+    parse_links
+    parse_css
+    parse_js
+    parse_images
+  end
+
+  def formatted_links
+    page_uri = URI(url)
+    @raw_links.map do |l|
+      URI.join(page_uri, URI(l))
+    end
+  end
+
+  def internal_links
+    formatted_links.select { |link| link.host == URI(url).host }
   end
 
   def parse_links
-    @links = @html.css('a').map { link['href'] }
+    @raw_links = @html.css('a').map { |a| a['href'] }
   end
 
   def parse_css
-    @css = @html.css('link').map { link['href'] }
+    @css = @html.css('link').map { |link| link['href'] }
   end
 
   def parse_js
-    @js = @html.css('script').map { link['src'] }
+    @js = @html.css("script[type='text/javascript']").map { |script| script['src'] }
   end
 
   def parse_images
-    @images = @html.css('img').map { link['src'] }
+    @images = @html.css('img').map { |img| img['src'] }
   end
 
   def open_url
-    open url
+    begin
+      open url
+    rescue OpenURI::HTTPError => error
+      ""
+    end
   end
 end
--- specs/web_crawler_specs.rb
+++ specs/web_crawler_specs.rb
@@ -5,10 +5,15 @@ require_relative "../lib/web_crawler"
 describe WebCrawler do
 
   describe '#parse' do
-    let(:url) { 'http://google.com' }
+    let(:url) { 'http://nokogiri.org' }
+    # let(:url) { 'http://google.com' }
 
     it "returns a crawler" do
       WebCrawler.crawl(url).must_be_instance_of WebCrawler
+    end
+
+    it "caches some pages" do
+      WebCrawler.crawl(url).pages.must_equal []
     end
   end
 end
@@ -16,10 +21,30 @@ end
 describe Page do
 
   describe '#parse' do
-    let(:url) { 'http://google.com' }
+    let(:url) { 'http://nokogiri.org' }
 
     it "returns a Page" do
       Page.parse(url).must_be_instance_of Page
     end
+
+    it "stores css" do
+      Page.parse(url).css.first.must_match /\.css/
+    end
+
+    it "stores js" do
+      Page.parse(url).js.first.must_match /\.js/
+    end
+
+    it "stores images" do
+      Page.parse(url).images.first.must_match /\.png/
+    end
+
+    it "internal links has no external links" do
+      Page.parse(url).internal_links.first.must_match /^\/$/
+    end
+
+    # it "stores links" do
+    #   Page.parse(url).links.must_equal ['jljlk']
+    # end
   end
 end
